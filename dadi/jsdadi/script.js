d3c4b32e4874789f4b8921dae0f88f543c5a8b2c
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-04 17:09:33 +0100

    feat: esercizio dadi finito

--- dadi/jsdadi/script.js
+++ dadi/jsdadi/script.js
@@ -1,4 +1,22 @@
-let dadoUser = 0;
-let dadoPc = 0;
+let dadoUser = Math.floor((Math.random() * 6) + 1);
+console.log(dadoUser);
+let dadoPc = Math.floor((Math.random() * 6) + 1);
+console.log(dadoPc);
 
-Math.random()
+
+ if (dadoPc>dadoUser) {
+     console.log(`Hai perso!, io ho tirato ${dadoPc} e tu ${dadoUser}`);
+ }
+ else if (dadoPc<dadoUser){
+     console.log(`Hai vinto!, io ho tirato ${dadoPc} e tu ${dadoUser}`);
+ }
+ else {
+     console.log(`Pareggio!, io ho tirato ${dadoPc} e tu ${dadoUser}`);
+ }
+    
+
+
+
+
+
+
